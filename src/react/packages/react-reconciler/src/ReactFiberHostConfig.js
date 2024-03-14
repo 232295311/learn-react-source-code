c968ffd285bd02e45bbfc708ba85279c1590c581
232295311 <232295311@qq.com>
2024-03-14 12:12:17 +0800

    feat: can debug react in the brower

--- src/react/packages/react-reconciler/src/ReactFiberHostConfig.js
+++ src/react/packages/react-reconciler/src/ReactFiberHostConfig.js
@@ -9,7 +9,7 @@
 
 /* eslint-disable react-internal/invariant-args */
 
-import invariant from 'shared/invariant';
+// import invariant from 'shared/invariant';
 
 // We expect that our Rollup, Jest, and Flow configurations
 // always shim this module with the corresponding host config
@@ -19,4 +19,6 @@ import invariant from 'shared/invariant';
 // sure that if we *do* accidentally break the configuration,
 // the failure isn't silent.
 
-invariant(false, 'This module must be shimmed by a specific renderer.');
+// invariant(false, 'This module must be shimmed by a specific renderer.');
+
+export * from './forks/ReactFiberHostConfig.dom';
